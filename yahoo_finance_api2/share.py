659ed9a19a939c17b81a3127674ff0052cb54f8e
Petr Kout <kout.petr@gmail.com>
2018-12-15 18:44:47 -0500

    Restructure code

--- yahoo_finance_api2/share.py
+++ yahoo_finance_api2/share.py
@@ -13,54 +13,6 @@ class Share(object):
         self.symbol = symbol
 
 
-    def _set_time_frame(self, periodType, period):
-        now = datetime.datetime.now()
-        if periodType == PERIOD_TYPE_DAY:
-            period = min(period, 59)
-            start_time = now - datetime.timedelta(days=period)
-        end_time = now
-
-        return start_time.strftime("%s"), end_time.strftime("%s")
-
-
-    def _download_symbol_data(self, period_type, period, 
-                              frequency_type, frequency):
-        start_time, end_time = self._set_time_frame(period_type, period)
-        url = (
-            'https://query1.finance.yahoo.com/v8/finance/chart/{0}?symbol={0}'
-            '&period1={1}&period2={2}&interval={3}&'
-            'includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&'
-            'region=US&crumb=t5QZMhgytYZ&corsDomain=finance.yahoo.com'
-        ).format(self.symbol, start_time, end_time, 
-                 self._get_frequency_str(frequency_type, frequency))
-
-        resp_json = requests.get(url).json()
-
-        if self._is_yf_response_error(resp_json):
-            self._print_yf_response_error(resp_json)
-            return
-
-        data_json = resp_json['chart']['result'][0]
-
-        return data_json
-
-
-    def _is_yf_response_error(self, resp):
-        return resp['chart']['error'] is not None
-
-
-    def _print_yf_response_error(self, resp):
-        raise YahooFinanceError(
-            '{0}: {1}'.format(
-                resp['chart']['error']['code'],
-                resp['chart']['error']['description']
-            )
-        )
-
-    def _get_frequency_str(self, frequency_type, frequency):
-        return '{1}{0}'.format(frequency_type, frequency)
-
-
     def get_historical(self, period_type, period, frequency_type, frequency):
         data = self._download_symbol_data(period_type, period, 
                                          frequency_type, frequency)
@@ -85,4 +37,51 @@ class Share(object):
             'volume': data['indicators']['quote'][0]['volume']
         }
 
-        return return_data
+        return return_data    
+    
+    def _set_time_frame(self, periodType, period):
+        now = datetime.datetime.now()
+        if periodType == PERIOD_TYPE_DAY:
+            period = min(period, 59)
+            start_time = now - datetime.timedelta(days=period)
+        end_time = now
+
+        return start_time.strftime("%s"), end_time.strftime("%s")
+
+
+    def _download_symbol_data(self, period_type, period, 
+                              frequency_type, frequency):
+        start_time, end_time = self._set_time_frame(period_type, period)
+        url = (
+            'https://query1.finance.yahoo.com/v8/finance/chart/{0}?symbol={0}'
+            '&period1={1}&period2={2}&interval={3}&'
+            'includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&'
+            'region=US&crumb=t5QZMhgytYZ&corsDomain=finance.yahoo.com'
+        ).format(self.symbol, start_time, end_time, 
+                 self._get_frequency_str(frequency_type, frequency))
+
+        resp_json = requests.get(url).json()
+
+        if self._is_yf_response_error(resp_json):
+            self._raise_yf_response_error(resp_json)
+            return
+
+        data_json = resp_json['chart']['result'][0]
+
+        return data_json
+
+
+    def _is_yf_response_error(self, resp):
+        return resp['chart']['error'] is not None
+
+
+    def _raise_yf_response_error(self, resp):
+        raise YahooFinanceError(
+            '{0}: {1}'.format(
+                resp['chart']['error']['code'],
+                resp['chart']['error']['description']
+            )
+        )
+
+    def _get_frequency_str(self, frequency_type, frequency):
+        return '{1}{0}'.format(frequency_type, frequency)
